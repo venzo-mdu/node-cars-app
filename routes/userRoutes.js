8023caef4d8ff2878169f25a46cfccde722c2c02
Samcaleb <samcalebjames2017@gmail.com>
2023-07-26 16:18:55 +0530

    base64 decode final

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -21,20 +21,47 @@ cloudinary.config({
     secure: true
   });
 
-router.post("/test",upload.single("image"),async (req,res,next)=>{
-    const result = await cloudinary.uploader.upload(req.file.path);
-    const imageUrl = result.secure_url;
-    console.log(req.body);
-    console.log(req.file);
-    await Test.create({
-    image:imageUrl,
+// router.post("/test",upload.single("image"),async (req,res,next)=>{
+//     const result = await cloudinary.uploader.upload(req.file.path);
+//     const imageUrl = result.secure_url;
+//     console.log(req.body);
+//     console.log(req.file);
+//     await Test.create({
+//     image:imageUrl,
 
-}).then((res)=>{
-    console.log("image and details saved success")
-}).catch((err)=>{
-    console.log(err,"error has occured")
+// }).then((res)=>{
+//     console.log("image and details saved success")
+// }).catch((err)=>{
+//     console.log(err,"error has occured")
+// });
+// res.send('Image and details saved successfully');
+// });
+// cloudinary.v2.uploader.upload_large(imagesExterieur[i], {
+//     folder: 'MecArtToyota/imagesExterieur',
+//     chunk_size = 6000000
+// });
+// router.use(express.json({limit:' 50mb'}));
+// router.use(express.urlencoded({limit:'50mb',extended:true}));
+router.post("/test",async (req,res)=>{
+    console.log(req.body.base64Image,"dta1");
+    try{
+
+        // const base64String = req.body.image;
+        // console.log(base64String);
+        console.log(req.body.base64Image,"dta2");
+        const uploadResult = await cloudinary.uploader.upload(req.body.base64Image,function(error,result){
+            console.log("reulst",result)
+            console.log("err",error)
+        })
+        res.json({ imageUrl :uploadResult.secure_url})
+        await Test.create({
+            image:uploadResult.secure_url
+        })
+    }catch(err){
+        console.log(err)
+        res.status(500).json({error:'something went wrong'})
+    }
 });
-res.send('Image and details saved successfully');
-});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 
 
 const port = process.env.PORT || 5000;
-
+// app.use(express.json({limit:' 50mb'}));
+// app.use(express.urlencoded({limit:'50mb',extended:true}))
 // app.use(fileUpload({
 //     useTempFiles:true
 // }))
